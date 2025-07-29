776108f82dc2e391de954306413cc725c7ee0ce0
fescii <isfescii@gmail.com>
2025-07-29 10:05:01 +0300

    Update meetings icon in FormationNav component
    - Replaced the existing SVG icon for meetings with a new design.
    - Adjusted the size of the SVG from 16x16 to 24x24 for better visibility.
    - The new icon features a more modern look while maintaining accessibility.

--- js/uis/navs/formtion.js
+++ js/uis/navs/formtion.js
@@ -714,10 +714,10 @@ export default class FormationNav extends HTMLElement {
       <li class="nav-item meetings" data-nav="meetings">
         <div class="link">
           <a href="/formation/meetings">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" color="currentColor" fill="none">
-              <path d="M21 10H16L15 7H9L8 10H3L4 16H20L21 10Z" stroke="currentColor" stroke-width="1.8" stroke-linejoin="round" />
-              <path d="M8 10V5C8 3.89543 8.89543 3 10 3H14C15.1046 3 16 3.89543 16 5V10" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" />
-              <path d="M8 16V19C8 20.1046 8.89543 21 10 21H14C15.1046 21 16 20.1046 16 19V16" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="currentColor" fill="none">
+              <path d="M11 8L13 8" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" />
+              <path d="M2 11C2 7.70017 2 6.05025 3.02513 5.02513C4.05025 4 5.70017 4 9 4H10C13.2998 4 14.9497 4 15.9749 5.02513C17 6.05025 17 7.70017 17 11V13C17 16.2998 17 17.9497 15.9749 18.9749C14.9497 20 13.2998 20 10 20H9C5.70017 20 4.05025 20 3.02513 18.9749C2 17.9497 2 16.2998 2 13V11Z" stroke="currentColor" stroke-width="1.8" />
+              <path d="M17 8.90585L17.1259 8.80196C19.2417 7.05623 20.2996 6.18336 21.1498 6.60482C22 7.02628 22 8.42355 22 11.2181V12.7819C22 15.5765 22 16.9737 21.1498 17.3952C20.2996 17.8166 19.2417 16.9438 17.1259 15.198L17 15.0941" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" />
             </svg>
             <span class="text">Meetings</span>
           </a>
